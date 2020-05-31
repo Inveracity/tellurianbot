b30fd2e14fbea9635d47c17f1ead6d1e169c43fb
Christopher Baklid <christopherb@unity3d.com>
2020-05-31 17:11:32 +0200

    if not sketch delete it

--- tellurian/bot.py
+++ tellurian/bot.py
@@ -1,6 +1,5 @@
 import logging
 import discord
-
 from config import Config
 
 logging.basicConfig(level=logging.ERROR, format='%(asctime)s [%(levelname)-7s] %(message)s')
@@ -9,22 +8,35 @@ log = logging.getLogger(__name__)
 config = Config()
 log.info(config.token)
 
+def bad_sketch(msg) -> bool:
+    """ If True then delete message """
+
+    if msg.channel.id == config.sketchbook:
+        if "https://www.dropbox.com/" in msg.content:
+            return False
+
+        if "https://drive.google.com/" in msg.content:
+            return False
+
+        if not msg.attachments:
+            return True
+
+        for attachment in msg.attachments:
+            if ".mp3" not in attachment.filename:
+                return True
+
+    return False
+
+
 class Tellurian(discord.Client):
 
     async def on_ready(self):
         log.info(f'Logged on as {self.user}')
 
     async def on_message(self, message):
-        """ Delete messages that are not mp3 files from #sketchbook """
-        if message.channel.id == config.sketchbook:
-            if not message.attachments:
-                await message.delete()
+        if bad_sketch(message):
+            await message.delete()
 
-            for attachment in message.attachments:
-                if ".mp3" not in attachment.filename:
-                    await message.delete()
-
-
-client = Tellurian()
-
-client.run(config.token)
+if __name__ == "__main__":
+    client = Tellurian()
+    client.run(config.token)
